2e2eaa4bb1d5a4ac703450306ef6482491ed6a68
Fahad Khalid <fahadkhalid210@gmail.com>
2020-03-10 17:05:30 +0500

    Update csv_merge.py

--- csv_merge.py
+++ csv_merge.py
@@ -1,7 +1,8 @@
 import os
 import glob
 import pandas as pd
-os.chdir("csv")
+#Change “csv_folder” to your desired working directory
+os.chdir("csv_folder")
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 combined_csv = pd.concat([pd.read_csv(f, encoding = "ISO-8859-1") for f in all_filenames ])
